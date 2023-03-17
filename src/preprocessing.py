fa397d7589dd91a86212684bbc8b43ab4b9b628c
Maksim Goncharov <goncharov.mv@phystech.edu>
2023-03-17 22:29:52 +0300

    change preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -15,18 +15,18 @@ def adaptive_thresholding(image, a=31, b=5):
     return cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, a, b)
 
 
-def padding(image, size=512):
+def padding(image, size=1024, inter=cv2.INTER_AREA):
     old_height, old_width = image.shape[0], image.shape[1]
     if old_height <= old_width:
         new_width = size
         new_height = size * old_height // old_width
-        image = cv2.resize(image, (new_width, new_height))
+        image = cv2.resize(image, (new_width, new_height), interpolation=inter)
         y = size - image.shape[0]
         image = cv2.copyMakeBorder(image, y // 2, y // 2 + y % 2, 0, 0, cv2.BORDER_CONSTANT)
     else:
         new_width = size * old_width // old_height
         new_height = size
-        image = cv2.resize(image, (new_width, new_height))
+        image = cv2.resize(image, (new_width, new_height), interpolation=inter)
         x = size - image.shape[1]
         image = cv2.copyMakeBorder(image, 0, 0, x // 2, x // 2 + x % 2, cv2.BORDER_CONSTANT)
     return image
