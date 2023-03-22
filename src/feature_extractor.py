ac6b288af07b7ac02ba3622e0e4e47377c9c2ce0
Alexey <azakkirov@gmail.com>
2023-03-22 22:22:20 +0300

    fix

--- src/feature_extractor.py
+++ src/feature_extractor.py
@@ -2,12 +2,8 @@ import cv2
 import numpy as np
 
 
-def linear_perspective(image, points, shape=None):
-    if shape is None:
-        height = np.max(np.sqrt(np.sum((points - np.roll(points, 1)) ** 2, axis=0)))
-        width = height
-    else:
-        height, width = shape
+def linear_perspective(image, points, shape=1024):
+    height, width = shape, shape
     output_points = np.float32([[0, 0], [0, height - 1], [width - 1, height - 1], [width - 1, 0]])
     input_points = np.float32(points)
     transformation = cv2.getPerspectiveTransform(input_points, output_points)
