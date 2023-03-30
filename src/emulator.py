7861b62a2917f6ea88e52dd48e674fbb0708c9ca
Evtushenko Dmitry <evtushenko.ds@phystech.edu>
2023-03-30 19:38:32 +0300

    [GOAB-25] board_scanner emulation implemented

--- src/emulator.py
+++ src/emulator.py
@@ -2,6 +2,7 @@ import cv2
 import time
 from board import Board
 
+
 def image_generator(filename):
     start_time = time.time()
     cap = cv2.VideoCapture(filename)
@@ -17,5 +18,22 @@ def image_generator(filename):
             return
 
 
-def board_generator(size, sleep_time):
-    pass
+class BoardGenerator:
+    __slots__ = [current_board]
+
+    def __init__(self):
+        self.current_board = board()
+
+    def __call__(self, size, sleep_time):
+        self.current_board.put_stone(0, 0, 1)
+        time.sleep(sleep_time)
+        yield self.current_board
+        self.current_board.put_stone(1, 1, -1)
+        time.sleep(sleep_time)
+        yield self.current_board
+        self.current_board.clear()
+        time.sleep(sleep_time)
+        yeild self.current_board
+
+
+board_generator = BoardGenerator()
