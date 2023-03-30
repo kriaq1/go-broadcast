a4a3e0b862370da00455afd5db1c6513f6a02640
Mikhail Petrov <petrov.m.a02@mail.ru>
2023-03-30 12:52:23 +0300

    [GOAB-22] add manual editor

---
+++ src/manual_editor.py
@@ -0,0 +1,21 @@
+from src.board import Board
+
+
+class ManualEditor:
+    def __init__(self):
+        self.board = Board()
+
+    def add(self, board):
+        self.board = board
+
+    def put_stone(self, x, y, color):
+        self.board._board[x][y] = color
+
+    def remove_stone(self, x, y):
+        self.board._board[x][y] = 0
+
+    def get(self):
+        return self.board
+
+    def send(self, api):
+        return api.add(self.board)
