f2a562428647ebcdb1cbd1aafbd0a7c92079908b
Maksim Goncharov <goncharov.mv@phystech.edu>
2023-04-23 12:29:33 +0300

    [GOAB-65] Change board show

--- tests/stream_recognition_test.py
+++ tests/stream_recognition_test.py
@@ -7,9 +7,10 @@ from src.stream_capture import VideoCapture
 from src.stream_recognition import StreamRecognition
 
 
-def show_board_state(image, board):
+def show_board_state(image, board, probabilities):
     cv2.imshow('image', image)
-    board = board[::-1]
+    board = board.copy()[::-1]
+    prob = probabilities.copy()[::-1]
     empty = np.zeros((608, 608, 3), np.uint8)
     empty[:, :] = (181, 217, 253)
     coords = np.linspace(28, 580, 19).astype(int)
@@ -20,6 +21,9 @@ def show_board_state(image, board):
         for j in range(len(coords)):
             x = coords[i]
             y = coords[j]
+            if prob[j][i] == 0:
+                cv2.circle(empty, (x, y), radius=12, color=(0, 0, 255), thickness=-1)
+                continue
             if board[j][i] == 1:
                 cv2.circle(empty, (x, y), radius=12, color=(255, 255, 255), thickness=-1)
             if board[j][i] == -1:
@@ -31,7 +35,7 @@ def show_board_state(image, board):
 if __name__ == '__main__':
     save_path_search = '../src/state_recognition/model_saves/segmentation18.pth'
     save_path_detect = '../src/state_recognition/model_saves/yolo8n.pt'
-    video_path = 'video/1.mp4'
+    video_path = 'video/2.mp4'
 
     device = 'cpu'
     # if you can use cuda:
@@ -45,4 +49,4 @@ if __name__ == '__main__':
         res, image = source.get(timestamp)
         print(quality, timestamp)
         # print(prob)
-        show_board_state(image, board)
+        show_board_state(image, board, prob)
