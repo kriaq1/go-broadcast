69cf5c26c5d1689479006a3066f3b8aac24ac3a6
Maksim Goncharov <goncharov.mv@phystech.edu>
2023-05-11 12:11:13 +0000

    update run.py for windows

--- run.py
+++ run.py
@@ -6,15 +6,23 @@ app = QApplication(sys.argv)
 from src.gui import Controller
 from src.gui import Window
 
-global_timestamp = Value('i', 0)
+import os
 
-controller = Controller(save_path_search="src/state_recognition/model_saves/segmentation18.pth",
-                        save_path_detect="src/state_recognition/model_saves/yolo8n_608_1200.pt",
-                        save_path_sgf='./sgf.sgf',
-                        device='cpu',
-                        global_timestamp=global_timestamp)
+if __name__ == '__main__':
+    path = os.path.abspath(__file__)
+    path = path.replace(os.sep, '/')
+    path = os.path.split(path)[0] + '/'
 
-window = Window(controller=controller, global_timestamp=global_timestamp, cache_path='temp/', fps_save=0.1)
-del controller
-window.show()
-sys.exit(app.exec_())
+    global_timestamp = Value('i', 0)
+
+    controller = Controller(save_path_search=path + "src/state_recognition/model_saves/segmentation18.pth",
+                            save_path_detect=path + "src/state_recognition/model_saves/yolo8n_608_1200.pt",
+                            save_path_sgf=path + './sgf.sgf',
+                            device='cpu',
+                            global_timestamp=global_timestamp)
+
+    window = Window(controller=controller, global_timestamp=global_timestamp, cache_path=path + 'temp/', fps_save=0.1)
+    del controller
+    window.show()
+    sys.exit(app.exec_())
+
