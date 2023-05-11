5ebb326b0c4f01bde7d4492f11cc3c4833e6e305
kriaq <azakkirov@gmail.com>
2023-05-11 22:06:07 +0300

    change palette

--- src/gui/widgets/info_widget.py
+++ src/gui/widgets/info_widget.py
@@ -5,6 +5,7 @@ class InfoWidget(QtWidgets.QTextEdit):
     def __init__(self, text=""):
         QtWidgets.QTextEdit.__init__(self)
         self.resize(800, 600)
+        self.setWindowTitle("Info")
         self.setStyleSheet("color:white;")
         self.setMinimumSize(QtCore.QSize(800, 600))
         self.setMaximumSize(QtCore.QSize(800, 600))
